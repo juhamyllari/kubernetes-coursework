3fc99e98310be1553bdb5cb5371bf5f6aa066050
Juha Myllari <jmyllari@gmail.com>
2026-07-15 10:40:05 +0300

    Exercise 1.5

--- the_project/todo_app.py
+++ the_project/todo_app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 import os
 import uvicorn
 
@@ -6,7 +7,22 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-	return {"message": "Todo app is running"}
+	return HTMLResponse(
+		content="""
+		<!doctype html>
+		<html lang="en">
+		  <head>
+		    <meta charset="utf-8" />
+		    <meta name="viewport" content="width=device-width, initial-scale=1" />
+		    <title>To Do app</title>
+		  </head>
+		  <body>
+		    <h1>Welcome to the To Do app</h1>
+		  </body>
+		</html>
+		""",
+		media_type="text/html",
+	)
 
 if __name__ == "__main__":
 	port = int(os.environ.get("PORT", 8000))
